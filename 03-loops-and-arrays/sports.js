f5a9700b49bb66ac6d0e0b57c392c5cd8d898c67
Prateek <prateek.verma0701@gmail.com>
2020-04-02 23:14:07 +0530

    putting condition inside loop

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -1,9 +1,10 @@
 var sportOne = ["Golf", "Cricket", "Tennis", "Swimming"];
-for (var i = 0; i < sportOne.length; i++) {
-    console.log(sportOne[i]);
-}
-// Let's use simplified for loop
 for (var _i = 0, sportOne_1 = sportOne; _i < sportOne_1.length; _i++) {
     var tempSport = sportOne_1[_i];
-    console.log("Sport " + tempSport);
+    if (tempSport == "Cricket") {
+        console.log("My favorite sport : " + tempSport);
+    }
+    else {
+        console.log("Sport : " + tempSport);
+    }
 }
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -1,11 +1,11 @@
 let sportOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 
-for (let i = 0; i < sportOne.length; i++) {
-    console.log(sportOne[i]);
-}
-
-// Let's use the simplified for loop
-
 for (let tempSport of sportOne) {
-    console.log("Sport " + tempSport);
+
+    if (tempSport == "Cricket") {
+        console.log("My favorite sport : " + tempSport)
+    }
+    else {
+        console.log("Sport : " + tempSport);
+    }
 }
