794b9f26d534f9f56d33975feed458a9d0039730
Prateek <prateek.verma0701@gmail.com>
2020-04-02 23:08:59 +0530

    looping through array of string - simplified loop

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -2,3 +2,8 @@ var sportOne = ["Golf", "Cricket", "Tennis", "Swimming"];
 for (var i = 0; i < sportOne.length; i++) {
     console.log(sportOne[i]);
 }
+// Let's use simplified for loop
+for (var _i = 0, sportOne_1 = sportOne; _i < sportOne_1.length; _i++) {
+    var tempSport = sportOne_1[_i];
+    console.log("Sport " + tempSport);
+}
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -3,3 +3,9 @@ let sportOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
 for (let i = 0; i < sportOne.length; i++) {
     console.log(sportOne[i]);
 }
+
+// Let's use the simplified for loop
+
+for (let tempSport of sportOne) {
+    console.log("Sport " + tempSport);
+}
