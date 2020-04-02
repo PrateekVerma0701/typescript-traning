91f5be37d7b8a1a8468bf9b0263b3ba30d6e2800
Prateek <prateek.verma0701@gmail.com>
2020-04-02 23:43:26 +0530

    creating a constructor

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,10 +1,11 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, thelast) {
+        this.firstName = theFirst;
+        this.lastName = thelast;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.firstName = "Martin";
-myCustomer.lastName = "Dixon";
+// create an istance
+var myCustomer = new Customer("Prateek", "Verma");
 console.log("First Name : " + myCustomer.firstName);
 console.log("Last Name : " + myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -2,12 +2,15 @@ class Customer {
 
     firstName: string;
     lastName: string;
+
+    constructor(theFirst: string, thelast: string) {
+        this.firstName = theFirst;
+        this.lastName = thelast;
+    }
 }
 
-let myCustomer = new Customer()
-
-myCustomer.firstName = "Martin";
-myCustomer.lastName = "Dixon";
+// create an istance
+let myCustomer = new Customer("Prateek", "Verma")
 
 console.log("First Name : " + myCustomer.firstName);
 console.log("Last Name : " + myCustomer.lastName);
