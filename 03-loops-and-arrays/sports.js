0f123789c4f50ce043e116b28d3f4de721bd5587
Prateek <prateek.verma0701@gmail.com>
2020-04-02 23:03:22 +0530

    looping through array of string - tradition way

---
+++ 03-loops-and-arrays/sports.js
@@ -0,0 +1,4 @@
+var sportOne = ["Golf", "Cricket", "Tennis", "Swimming"];
+for (var i = 0; i < sportOne.length; i++) {
+    console.log(sportOne[i]);
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,5 @@
+let sportOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let i = 0; i < sportOne.length; i++) {
+    console.log(sportOne[i]);
+}
