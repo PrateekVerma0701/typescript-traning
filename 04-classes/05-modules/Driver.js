33e37aa017c70f5d82e58d72ee7acfae088e831a
Prateek <prateek.verma0701@gmail.com>
2020-05-03 00:19:24 +0530

    modules - import and export

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customer_1 = require("./Customer");
+// create an instance
+var myCustomer = new Customer_1.Customer("Prateek", "Verma");
+console.log("First Name : " + myCustomer.firstName);
+console.log("Last Name : " + myCustomer.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import { Customer } from "./Customer";
+
+// create an instance
+let myCustomer = new Customer("Prateek", "Verma")
+
+console.log("First Name : " + myCustomer.firstName);
+console.log("Last Name : " + myCustomer.lastName);
